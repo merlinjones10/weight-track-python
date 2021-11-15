45b5d7a28576899613a65e006fbb926157d4d228
merlinjones10 <merlinjones10@gmail.com>
2021-11-15 18:33:05 +0000

    dummy data script added

---
+++ dummyData.py
@@ -0,0 +1,32 @@
+import sqlite3
+
+conn = sqlite3.connect('weights.db')
+
+try:
+    conn = sqlite3.connect('weights.db')
+    print("Connection successful")
+except:
+    print('Failed to connect')
+
+c = conn.cursor()
+
+#records or rows in a list
+weightRecords = [
+                ('15/11/2021', 69),
+                ('15/11/2021', 69),
+                ('15/11/2021', 69),
+                ('15/11/2021', 69),
+                ('15/11/2021', 69),
+                ('15/11/2021', 69),
+                ('15/11/2021', 69),
+                ]
+
+#insert multiple records in a single query
+c.executemany('INSERT INTO weights VALUES(?,?);',weightRecords);
+
+print('We have inserted', c.rowcount, 'records to the table.')
+
+#commit the changes to db
+conn.commit()
+#close the connection
+conn.close()
--- main.py
+++ main.py
@@ -1,35 +1,44 @@
 from datetime import date
 import sqlite3
+# 79kg
+# GLOBAL VARIABLES
 conn = sqlite3.connect('weights.db')
 c = conn.cursor()
-today = date.today().strftime("%m/%d/%Y")
+today = date.today().strftime("%d/%m/%Y")
 dayInWeek = date.today().strftime("%a")
 
-
+print(today)
+#  CLASSES
 class Measurement:
-  def __init__(self, weight):
-    self.weight = weight
-    self.date = today
+    def __init__(self, weight):
+        self.weight = weight
+        self.date = today
 
+
+# FUNCTIONS
 def calcAvg(weights):
     return round(sum(weights) / 7, 1)
+
 
 def kgToLbs(kgs):
     pounds = kgs * 2.2046
     return pounds
+
+
 def takeMeasurement():
-    # yOrN = ''
     todaysMeasurement = int(input("Enter today's weight: "))
     currentWeight = Measurement(todaysMeasurement)
     c.execute("INSERT INTO weights VALUES (?, ?)", (currentWeight.date, currentWeight.weight))
+    conn.commit()
     print('Entry successful')
 
+
 def displayAverage():
-    yOrN = input('Show weeks weights and average? (y/n)')
-    if yOrN == 'n':
-        pass
+    if input('Show weeks weights and average? (y/n): ').lower() == 'n':
+        print('See you tomorrow')
     else:
         displayEach()
+
 
 def displayEach():
     c.execute("SELECT * FROM weights")
@@ -41,25 +50,21 @@ def displayEach():
     for item in items:
         print(f"-{item[0]}  \t - {item[1]}kg")
     print(f"\nAverage: {averageWeight}kg --- or --- {lbsConversion}lbs ")
-# ------------ PROGRAM FLOW ---------------
+
+
+# ------------ PROGRAM FLOW --------------- #
 takeMeasurement()
-displayAverage()
-# kgToLbs()
-conn.commit()
+# displayAverage()
 conn.close()
+
+#  ------------- fragments ------------ #
 # c.execute("INSERT INTO weights VALUES ('date', 23)")
-
-
 # take_measurement()
 # displayEach()
-
 # sample_week_weights = [12, 13, 12, 14, 14, 12, 11]
 #
 # if 'Fri' == 'Fri':
 #     avg = calcAvg(sample_week_weights)
 #     print(f"Average weight is {avg}")
-
-
 # c.execute(("CREATE TABLE weights(date TEXT, weight INT)"))
 # c.execute("insert into contacts values(?,?,?)",(name, phone, email))
-
